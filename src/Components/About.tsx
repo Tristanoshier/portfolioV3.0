d4218239299139f3ee93545267ed5a928a5f4cf8
Tristanoshier <tsoshier@bsu.edu>
2020-07-06 15:25:21 -0400

    add styling toabout section

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,12 +4,12 @@ export const About = () => {
     return (
         <div id="about" className="about">
             <section className="about-title">
-            <h1 className="main-header">About</h1>
+                <h1 className="main-header">About</h1>
             </section>
             
             <section className="about-section">
                 <p className="about-text">I'm currently a full-stack developer seeking a position in Software Development/Web Development that will challenge me and further my coding skills. I enjoy problem-solving, creative thinking, and am very adaptable to any team environment. Throughout past educational and work experiences, I have met strict project deadlines, developed unique floor plans, and have worked in many different team environments. All of these experiences have prepared me for a multitude of situations I will face on the job in the tech world.</p>
-                <h1 className="about-img">Image of me</h1>
+                <img className="about-img" src={require("../assets/Tristan.jpg")} alt="picture of me" />
             </section>
         </div>
     )
--- src/Components/Landing.tsx
+++ src/Components/Landing.tsx
@@ -5,7 +5,7 @@ export const Landing = () => {
 
     useEffect(() => {
         const tl = new TimelineMax();
-        tl.fromTo(".hero", 1, {height: "0%"}, {height: "80%"})
+        tl.fromTo(".hero", 1, {height: "0%"}, {height: "85%"})
         .fromTo(".hero", 1.2, {width: "100%"}, {width: "80%"})
         .fromTo(".slider", 1.2, {x: "-100%"}, {x: "0%"}, "-=1.2") //the -=1.2 tells this animation to happen at the same time as the line above, for a smoother transition
         .fromTo("#logo", 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
