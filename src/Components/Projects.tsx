0612da678037ec5b2acf105a9df91c369dce97f2
Tristanoshier <tsoshier@bsu.edu>
2020-07-15 19:25:58 -0400

    add project section

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDog, faSpaceShuttle, faRecordVinyl } from '@fortawesome/free-solid-svg-icons';
 
 export const Projects = () => {
     return (
-        <div className="projects">
+        <div id="projects" className="projects">
             <section className="about-title">
                 <h1 className="main-header">Projects</h1>
             </section>
-            
+            <div className="project-section">
+                <div className="app-layout">
+                    <a href="https://tat-petpals-client.herokuapp.com/" target="_blank">
+                        <div className="pet-app">
+                            <FontAwesomeIcon className="pet-app-icon" icon={faDog} size="4x"></FontAwesomeIcon>
+                        </div>
+                    </a>
+                    <p className="app-name">PetPals</p>
+                </div>
+                <div className="app-layout">
+                    <a href="https://solarsystemvisualizer-75b91.firebaseapp.com/" target="_blank">
+                        <div className="solar-app">
+                            <FontAwesomeIcon className="solar-app-icon" icon={faSpaceShuttle} size="3x"></FontAwesomeIcon>
+                        </div>
+                    </a>
+                    <p className="app-name">Solar System Visualizer</p>
+                </div>
+                <div className="app-layout">
+                    <a href="https://to-myrecords-client.herokuapp.com/" target="_blank">
+                        <div className="record-app">
+                            <FontAwesomeIcon className="record-app-icon" icon={faRecordVinyl} size="4x"></FontAwesomeIcon>
+                        </div>
+                    </a>
+                    <p className="app-name">MyRecords</p>
+                </div>
+            </div>
         </div>
     )
 }
